65e30eceacafb8c41df7b50a4acaabd8dc8adae5
Maxime-Mauny <m.mauny@hotmail.fr>
2019-05-04 22:36:03 +0200

    template-parts et functions.php ok

--- content/themes/osailing/home.php
+++ content/themes/osailing/home.php
@@ -1,71 +1,23 @@
 <?php
 // Je demande à WP de me charger mon header
 get_header();
-?>
 
-    <section class="articles" id="articles">
-      <article class="article">
-        <img src="https://source.unsplash.com/800x600/?people" alt="" class="article__img">
-        <div class="article__content">
-          <h2 class="article__content__title">
-            Une rencontre inattendue
-          </h2>
-          <p class="article__content__text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo esse dolore voluptas exercitationem numquam reiciendis dicta inventore assumenda
-          </p>
-          <a href="" class="button">Lire la suite</a>
-        </div>
-      </article>
-      <article class="article">
-        <img src="https://source.unsplash.com/800x600/?sea,food" alt="" class="article__img">
-        <div class="article__content">
-          <h2 class="article__content__title">
-            Escapade gourmande
-          </h2>
-          <p class="article__content__text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo esse dolore voluptas exercitationem numquam reiciendis dicta inventore assumenda
-          </p>
-          <a href="" class="button">Lire la suite</a>
-        </div>
-      </article>
-      <article class="article">
-        <img src="https://source.unsplash.com/800x600/?surf" alt="" class="article__img">
-        <div class="article__content">
-          <h2 class="article__content__title">
-            Le paradis ?
-          </h2>
-          <p class="article__content__text">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo esse dolore voluptas exercitationem numquam reiciendis dicta inventore assumenda
-          </p>
-          <a href="" class="button">Lire la suite</a>
-        </div>
-      </article>
-    </section>
 
-    <section class="values" id="values">
-      <div class="values__list">
-        <div class="value">
-          <h3 class="value__title">Escales</h3>
-          <p class="value__content">19</p>
-        </div>
-        <div class="value">
-          <h3 class="value__title">Semaines en mer</h3>
-          <p class="value__content">123</p>
-        </div>
-        <div class="value">
-          <h3 class="value__title">Rencontres</h3>
-          <p class="value__content">214</p>
-        </div>
-      </div>
-      <div class="values__text">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi enim fugit ratione dolore inventore perspiciatis qui labore cum modi, dicta consequuntur ipsa amet, earum nam minus at unde atque?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi enim fugit ratione dolore inventore perspiciatis qui labore cum modi, dicta consequuntur ipsa amet, earum nam minus at unde atque?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi enim fugit ratione dolore inventore perspiciatis qui labore cum modi, dicta consequuntur ipsa amet, earum nam minus at unde atque?</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero nisi enim fugit ratione dolore inventore perspiciatis qui labore cum modi, dicta consequuntur ipsa amet, earum nam minus at unde atque?</p>
-      </div>
-    </section>
+// Je demande à WP si j'ai du contenu
+// je dit à WP alors tant que j'ai du contenu
+// alors fait the_post() !
+if (have_posts()): while(have_posts()): the_post();
 
-<?php
+    // J'inclu mon "fragment" de template qui génère
+    // le code HTML d'un extrait d'article
+    get_template_part('template-parts/post/article-excerpt');
+
+endwhile; endif;
+
+
+get_template_part('template-parts/stats/stats');    
+
+
 // Je demande à WP de me charger mon footer
 get_footer();
 ?>
